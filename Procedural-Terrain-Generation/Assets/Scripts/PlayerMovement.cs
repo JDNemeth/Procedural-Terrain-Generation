42115d08f74691352dc6b7c0f7c7df9299a7091d
Ján Daniel Németh <jandanielnemeth@gmail.com>
2020-12-28 22:48:53 +0100

    adding project

---
+++ Procedural-Terrain-Generation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public Transform groundCheck;
+    private float groundDistance = 0.2f;
+    public LayerMask enviroment;
+    private bool isGrounded = true;
+
+
+    private float accelerationForce = 300f;
+    private float jumpMultiplier = 20f;
+
+    private float maxSpeed = 2f;
+    private float playerMaxSpeed = 2f;
+    private float sprintMultiplier = 2f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //pohyb po ploche s obmedzenim maximalnej rychlosti pohybu
+        //W
+        if (Input.GetKey(KeyCode.W) && maxSpeed > rb.velocity.z)
+        {
+            rb.AddRelativeForce(accelerationForce * new Vector3(0, 0, 1f));
+        }
+
+
+        //A
+        if (Input.GetKey(KeyCode.A) && maxSpeed > -rb.velocity.x)
+        {
+            rb.AddRelativeForce(accelerationForce * new Vector3( - 1f, 0, 0));
+        }
+
+
+        //S
+        if (Input.GetKey(KeyCode.S) && maxSpeed > -rb.velocity.z)
+        {
+            rb.AddRelativeForce(accelerationForce * new Vector3(0, 0, - 0.7f));
+        }
+
+
+        //D
+        if (Input.GetKey(KeyCode.D) && maxSpeed > rb.velocity.x)
+        {
+            rb.AddRelativeForce(accelerationForce * new Vector3( 1f, 0, 0));
+        }
+
+
+        //SPRINT zvysi rychlost pohybu sprintmultiplier
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = playerMaxSpeed * sprintMultiplier;
+            accelerationForce = 600f;
+        }
+        else
+        {
+            maxSpeed = 2f;
+            accelerationForce = 300f;
+        }
+    }
+    private void Update()
+    {
+        //JUMP
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, enviroment);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(accelerationForce * jumpMultiplier * new Vector3(0, 1f, 0));
+        }
+    }
+}
